d556d658a8fe3f1fe67255541d27b500f3a02342
jsusilo <enr@bk.ru>
2021-01-31 00:00:46 +0500

    app routes, directives

---
+++ resources/assets/js/app.js
@@ -0,0 +1,12 @@
+angular.module("todoApp", [
+  "ngRoute",
+  "ngResource",
+  "ngStorage",
+  "appRoutes",
+  "enterStroke",
+  "MainController",
+  "TodoController",
+  "UserController",
+  "UserService",
+  "TodoService",
+]);
