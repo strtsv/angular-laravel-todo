da686def6aa3771c32e080ad14531e154316f141
jsusilo <enr@bk.ru>
2021-01-30 23:59:53 +0500

    TodoService

---
+++ resources/assets/js/services/TodoService.js
@@ -0,0 +1,16 @@
+angular.module("TodoService", []).factory("Todo", [
+  "$resource",
+  function ($resource) {
+    return $resource(
+      "/api/todo/:todoId",
+      {
+        todoId: "@id",
+      },
+      {
+        update: {
+          method: "PUT",
+        },
+      }
+    );
+  },
+]);
